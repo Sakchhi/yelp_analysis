3b9227d0e8ee96a8a7cc62830dd3796b010fec2a
Sakchhi Srivastava <sakchhi.sri@gmail.com>
2020-01-25 10:10:28 +0530

    Iteration v0.5 -- BoW N-gram

--- run_config.py
+++ run_config.py
@@ -1,7 +1,7 @@
 import datetime
 
 model_date_to_read = '20200125'
-model_version_to_read = '0.4'
+model_version_to_read = '0.6'
 
 model_date_to_write = datetime.datetime.today().strftime('%Y%m%d')
-model_version_to_write = '0.5'
+model_version_to_write = '0.6'
